26f16c2dae1f6ecc98c3f5859120431bb5c01293
Kauê Gatto <kauegatto123@gmail.com>
2022-02-14 12:41:22 -0300

    refactor: http helper class added

---
+++ src/presentation/helpers/HttpHelper.ts
@@ -0,0 +1,39 @@
+import HttpResponse from '../interfaces/protocols/HttpResponse'
+
+export class HttpHelper { // no keyword for final, using private constructor instead
+  private constructor () {}
+  public static badRequest (error: Error): HttpResponse {
+    return {
+      statusCode: 400,
+      body: error
+    }
+  }
+
+  public static unauthorized (error: Error): HttpResponse {
+    return {
+      statusCode: 401,
+      body: error
+    }
+  }
+
+  public static ok (message: string): HttpResponse {
+    return {
+      statusCode: 200,
+      body: message
+    }
+  }
+
+  public static noContent (message: string): HttpResponse {
+    return {
+      statusCode: 204,
+      body: message
+    }
+  }
+
+  public static created<T> (entity: T): HttpResponse {
+    return {
+      statusCode: 204,
+      body: entity
+    }
+  }
+}
