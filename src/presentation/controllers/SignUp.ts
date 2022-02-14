2f4f52295e3390b544d6d65886234883fbb8e171
Kauê Gatto <kauegatto123@gmail.com>
2022-02-14 11:52:32 -0300

    test: Added best test coverage on signup

--- src/presentation/controllers/SignUp.ts
+++ src/presentation/controllers/SignUp.ts
@@ -8,6 +8,38 @@ export class SignUpController {
         }
       };
     }
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400,
+        body: {
+          error: 'email is missing'
+        }
+      };
+    }
+    if (!httpRequest.body.password) {
+      return {
+        statusCode: 400,
+        body: {
+          error: 'password is missing'
+        }
+      };
+    }
+    if (!httpRequest.body.passwordConfirmation) {
+      return {
+        statusCode: 400,
+        body: {
+          error: 'password confirmation is missing'
+        }
+      };
+    }
+    if (!(httpRequest.body.password === httpRequest.body.passwordConfirmation)) {
+      return {
+        statusCode: 400,
+        body: {
+          error: 'password and password confirmation are different'
+        }
+      };
+    }
     return {
       statusCode: 201,
       body: {
--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -14,6 +14,58 @@ describe('SignUp Controller', () => {
     const httpResponse = sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
   });
+  test('Should return 400 if no email is provided ', () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: '',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+  });
+  test('Should return 400 if no password is provided ', () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'email@gmail.com',
+        password: '',
+        passwordConfirmation: 'pass'
+      }
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+  });
+  test('Should return 400 if no password confirmation is provided ', () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'email@gmail.com',
+        password: 'any_password',
+        passwordConfirmation: ''
+      }
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+  });
+  test('Should return 400 if password and password and password confirmation are different ', () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'email@gmail.com',
+        password: 'any_password',
+        passwordConfirmation: 'another pass'
+      }
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+  });
   test('Should return 201 if valid body is provided ', () => {
     const sut = new SignUpController();
     const httpRequest = {
@@ -25,6 +77,7 @@ describe('SignUp Controller', () => {
       }
     };
     const httpResponse = sut.handle(httpRequest);
+    console.log(httpResponse.body);
     expect(httpResponse.statusCode).toBe(201);
   });
 });
