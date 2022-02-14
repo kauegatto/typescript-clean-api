1c1fc4d27a5a5efe887558bfd1725ec3e950a539
Kauê Gatto <kauegatto123@gmail.com>
2022-02-14 09:57:38 -0300

    chore: added jest and sample test

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+export default {
+  roots: ['<rootDir>/src'],
+  // Automatically clear mock calls, instances and results before every test
+  clearMocks: true,
+  // Indicates whether the coverage information should be collected while executing the test
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  testEnvironment: 'jest-environment-node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
